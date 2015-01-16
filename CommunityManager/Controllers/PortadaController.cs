f04d8c2f84ef96032444c43d8fbf0a545b8f06b0
moodswing <rob.arav@gmail.com>
2015-01-16 12:02:52 -0800

    portada

---
+++ CommunityManager/Controllers/PortadaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommunityManager.Controllers
+{
+    public class PortadaController : Controller
+    {
+        //
+        // GET: /Portada/
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+	}
+}
