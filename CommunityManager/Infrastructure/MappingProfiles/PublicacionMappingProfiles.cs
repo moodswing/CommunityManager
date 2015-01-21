7c1295f789e2a24ece5d4e4dfa1aa205b944b5d0
moodswing <rob.arav@gmail.com>
2015-01-21 12:27:27 -0800

    portada

---
+++ CommunityManager/Infrastructure/MappingProfiles/PublicacionMappingProfiles.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using CommunityManager.Models;
+using CommunityManager.ViewModels.Acceso;
+using CommunityManager.ViewModels.Portada;
+
+namespace CommunityManager.Infrastructure.MappingProfiles
+{
+    public class PublicacionMappingProfiles : Profile
+    {
+        private const string ViewModel = "PublicacionViewModel";
+
+        public override string ProfileName
+        {
+            get { return ViewModel; }
+        }
+
+        protected override void Configure()
+        {
+            CreateMap<PublicacionViewModel, Publicacion>()
+                    .ForMember(dest => dest.Titulo, opt => opt.MapFrom(src => src.Titulo))
+                    .ForMember(dest => dest.Descripcion, opt => opt.MapFrom(src => src.Descripcion))
+                    .ForMember(dest => dest.FechaIngreso, opt => opt.MapFrom(src => src.FechaIngreso))
+                    .IgnoreAllNonExisting()
+                    .ReverseMap()
+                    .ForMember(dest => dest.Titulo, opt => opt.MapFrom(src => src.Titulo))
+                    .ForMember(dest => dest.Descripcion, opt => opt.MapFrom(src => src.Descripcion))
+                    .ForMember(dest => dest.FechaIngreso, opt => opt.MapFrom(src => src.FechaIngreso))
+                    .IgnoreAllNonExistingSource();
+
+            CreateMap<ResumenPublicacionViewModel, Publicacion>()
+                    .ForMember(dest => dest.Titulo, opt => opt.MapFrom(src => src.Titulo))
+                    .ForMember(dest => dest.ID, opt => opt.MapFrom(src => src.Id))
+                    .ForMember(dest => dest.FechaIngreso, opt => opt.MapFrom(src => src.FechaIngreso))
+                    .ForMember(dest => dest.TipoPublicacion, opt => opt.MapFrom(src => src.TipoPublicacion))
+                    .IgnoreAllNonExisting()
+                    .ReverseMap()
+                    .ForMember(dest => dest.Titulo, opt => opt.MapFrom(src => src.Titulo))
+                    .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ID))
+                    .ForMember(dest => dest.FechaIngreso, opt => opt.MapFrom(src => src.FechaIngreso))
+                    .ForMember(dest => dest.TipoPublicacion, opt => opt.MapFrom(src => src.TipoPublicacion))
+                    .IgnoreAllNonExistingSource();
+        }
+    }
+}
